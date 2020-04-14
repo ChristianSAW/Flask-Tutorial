da7f01051f04a471b3926338fdbc1ec081464ba7
Christian Welling <csw73@cornell.edu>
2020-04-13 18:58:29 -0700

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -27,6 +27,9 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     # a simple home page 
     @app.route('/')
     def home():
